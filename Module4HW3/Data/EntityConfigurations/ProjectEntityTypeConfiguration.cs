c661ab333e95457be06a88f997e10489efc25537
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2022-01-23 12:46:01 +0200

    Add project files.

---
+++ Module4HW3/Data/EntityConfigurations/ProjectEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW3.Data.Entities;
+
+namespace Module4HW3.Data.EntityConfigurations
+{
+    public class ProjectEntityTypeConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.ToTable("Project").HasKey(p => p.Id);
+
+            builder.Property(p => p.Id).HasColumnName("ProjectId").ValueGeneratedOnAdd();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.Budget).IsRequired().HasColumnType("money");
+            builder.Property(p => p.StartedDate).IsRequired().HasMaxLength(7);
+        }
+    }
+}
